daa1e0a64b3a074800d202f48fbe0982e549ab13
jhh678 <jhh678878@163.com>
2018-07-09 22:42:14 +0800

    初始代码提交

---
+++ .temp/pages/recycle-list-demo.js
@@ -0,0 +1,3 @@
+import App from '..\\..\\src\\pages\\recycle-list-demo.vue'
+App.el = '#root'
+new Vue(App)
---
+++ src/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo" />
+    <text class="greeting">The environment is ready!</text>
+    <HelloWorld/>
+    <wxc-button text="Open Popup" @wxcButtonClicked="buttonClicked"></wxc-button>
+    <wxc-popup width="500" pos="left" :show="isShow" @wxcPopupOverlayClicked="overlayClicked"></wxc-popup>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  import {
+    WxcButton,
+    WxcPopup
+  } from 'weex-ui'
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      WxcButton,
+      WxcPopup
+    },
+    data() {
+      return {
+        logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
+        isShow: false
+      }
+    },
+    methods: {
+      buttonClicked() {
+        this.isShow = true
+      },
+      overlayClicked() {
+        this.isShow = false
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+
+</style>
