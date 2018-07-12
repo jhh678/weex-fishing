1799e4126701ff8cf620e2959450b63efe834f18
jianghaihua <jhh678878@163.com>
2018-07-12 18:29:58 +0800

    提交修改

---
+++ src/js/router/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by jhh678 on 2018/7/12.
+ */
+import Router from 'vue-router'
+import ViewHome from '../../views/home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  // mode: 'abstract',
+  routes: [{
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: ViewHome
+    }
+  ]
+})
---
+++ src/views/home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <text>主页</text>
+    <wxc-button text="Open Popup" @wxcButtonClicked="buttonClicked">
+    </wxc-button>
+    <wxc-popup width="500" pos="left" :show="isShow" @wxcPopupOverlayClicked="overlayClicked">
+    </wxc-popup>
+  </div>
+</template>
+
+<script>
+  import {
+    WxcButton,
+    WxcPopup
+  } from 'weex-ui'
+  // 或者分开引用（不是很推荐）
+  // import WxcButton from 'weex-ui/packages/wxc-button';
+  // import WxcPopup from 'weex-ui/packages/wxc-popup';
+  module.exports = {
+    components: {
+      WxcButton,
+      WxcPopup
+    },
+    data: () => ({
+      isShow: false
+    }),
+    methods: {
+      buttonClicked() {
+        this.isShow = true
+      },
+      overlayClicked() {
+        this.isShow = false
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+
+
+</style>
